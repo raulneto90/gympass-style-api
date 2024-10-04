6259865605f4cd0e99717e34f243e8db17ea0ffb
Raul Neto <raulneto90@gmail.com>
2024-10-04 09:44:20 -0300

    feat: add environment variables validation

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { app } from "./app";
+import { env } from "./modules/common/config/env";
 
 app.listen({
-  port: 3333,
+  port: env.PORT,
   host: '0.0.0.0'
 }).then(() => {
   console.log('🚀 Server started at http://localhost:3333')
