7e385368ee46d65cf393db16638687a179e3e36a
Raul Neto <raulneto90@gmail.com>
2024-10-07 17:45:09 -0300

    implemented rest client for api testing

--- src/modules/common/interface/routes/routes.ts
+++ src/modules/common/interface/routes/routes.ts
@@ -1,8 +1,10 @@
 import { CreateUserController } from '@api/modules/users/interface/controllers/CreateUserController';
 import { FastifyInstance } from 'fastify';
+import { FastifyPluginCallback } from 'fastify';
 
 const createUserController = new CreateUserController();
 
-export const routes = (app: FastifyInstance) => {
+export const routes: FastifyPluginCallback = (app: FastifyInstance, _opts, done) => {
 	app.post('/users', createUserController.handle);
+	done();
 };
