6ff5f679d1c735a81e86820edd7de6971e53ca46
Raul Neto <raulneto90@gmail.com>
2025-01-14 08:55:28 -0300

    test: add unit tests for AuthenticateUseCase to validate user authentication logic

---
+++ src/modules/users/application/use-cases/authenticate.unit.test.ts
@@ -0,0 +1,72 @@
+import { InvalidCredentialsError } from '@src/modules/common/errors/invalid-credentials.error';
+import jwt from 'jsonwebtoken';
+import { Mock, beforeEach, describe, expect, it, vi } from 'vitest';
+import { UsersRepository } from '../../domain/repositories/users.repository';
+import { PasswordHash } from '../../domain/services/password-hash';
+import { TokenGeneratorService } from '../../domain/services/token-generator';
+import { InMemoryUsersRepository } from '../../infraestructure/repositories/in-memory-users.repository';
+import { BcryptPasswordHash } from '../../infraestructure/services/bcrypt-password-hash';
+import { JsonWebTokenService } from '../../infraestructure/services/jsonwebtoken.service';
+import { AuthenticateUseCase } from './authenticate.usecase';
+
+vi.mock('jsonwebtoken');
+
+describe('AuthenticateUseCase', () => {
+	let tokenGeneratorService: TokenGeneratorService;
+	let passwordHash: PasswordHash;
+	let repository: UsersRepository;
+	let useCase: AuthenticateUseCase;
+
+	beforeEach(() => {
+		repository = new InMemoryUsersRepository();
+		passwordHash = new BcryptPasswordHash();
+		tokenGeneratorService = new JsonWebTokenService();
+		useCase = new AuthenticateUseCase(
+			repository,
+			passwordHash,
+			tokenGeneratorService,
+		);
+	});
+
+	it('should authenticate a user', async () => {
+		const user = await repository.create({
+			name: 'John Doe',
+			email: 'johndoe@example.com',
+			password: passwordHash.hash('password'),
+		});
+
+		(jwt.sign as Mock).mockReturnValue('token');
+
+		const result = await useCase.execute({
+			email: user.email,
+			password: 'password',
+		});
+
+		expect(result).toBeDefined();
+		expect(result).toHaveProperty('token');
+	});
+
+	it('should throw an error if the user does not exist', async () => {
+		await expect(
+			useCase.execute({
+				email: 'some-email@example.com',
+				password: 'password',
+			}),
+		).rejects.toBeInstanceOf(InvalidCredentialsError);
+	});
+
+	it('should throw an error if user password does not match', async () => {
+		const user = await repository.create({
+			name: 'John Doe',
+			email: 'johndoe@example.com',
+			password: passwordHash.hash('password'),
+		});
+
+		await expect(
+			useCase.execute({
+				email: user.email,
+				password: 'wrong-password',
+			}),
+		).rejects.toBeInstanceOf(InvalidCredentialsError);
+	});
+});
