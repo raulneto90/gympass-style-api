72a7d0889a947f1ec0bce779e4ded71a753c4756
Raul Neto <raulneto90@gmail.com>
2025-02-06 20:48:30 -0300

    feat: implement authentication test for AuthenticateUserController

--- src/modules/users/interfaces/controllers/authenticate-user-controller.test.ts
+++ src/modules/users/interfaces/controllers/authenticate-user-controller.test.ts
@@ -1,5 +1,21 @@
-import { describe, it } from 'vitest';
+import { app } from '@src/app';
+import request from 'supertest';
+import { describe, expect, it } from 'vitest';
 
 describe('AuthenticateUserController', () => {
-	it('ok', () => {});
+	it('should be able to authenticate', async () => {
+		await request(app.server).post('/v1/users').send({
+			name: 'John Doe',
+			email: 'johndoe@example.com',
+			password: '123456',
+		});
+
+		const response = await request(app.server).post('/v1/sessions').send({
+			email: 'johndoe@example.com',
+			password: '123456',
+		});
+
+		expect(response.statusCode).toBe(200);
+		expect(response.body).toEqual({ token: expect.any(String) });
+	});
 });
