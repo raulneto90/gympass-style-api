bae447956ea12817a4238e328f0ef8eb033a403d
Raul Neto <raulneto90@gmail.com>
2025-02-06 20:39:20 -0300

    feat: add dotenv dependency and configure environment variables for Prisma setup

--- prisma/vitest-environment-prisma/vitest-environment-prisma.ts
+++ prisma/vitest-environment-prisma/vitest-environment-prisma.ts
@@ -1,14 +1,41 @@
+import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+import { execSync } from 'node:child_process';
+import { randomUUID } from 'node:crypto';
 import { Environment } from 'vitest/environments';
+
+const prisma = new PrismaClient();
+
+function generateDatabaseUrl(schema: string) {
+	if (!process.env.DATABASE_URL) {
+		throw new Error('Please provide a DATABASE_URL environment variable');
+	}
+
+	const url = new URL(process.env.DATABASE_URL);
+
+	url.searchParams.set('schema', schema);
+
+	return url.toString();
+}
 
 export default (<Environment>{
 	name: 'prisma',
 	transformMode: 'ssr',
 	async setup() {
-		console.log('Executando setup do ambiente prisma');
+		const schema = randomUUID();
+		const databaseUrl = generateDatabaseUrl(schema);
+
+		process.env.DATABASE_URL = databaseUrl;
+
+		execSync('npx prisma migrate deploy');
 
 		return {
-			teardown() {
-				console.log('Executando setup do ambiente prisma');
+			async teardown() {
+				await prisma.$executeRawUnsafe(
+					`DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+				);
+
+				prisma.$disconnect;
 			},
 		};
 	},
